a535102729403d58b7c4c2158bfd48aeaa1e45d1
dev <yasinadam@outlook.com>
2017-05-10 19:01:38 +0100

    mon

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
     //'url' : 'mongodb://admin:GQcSZN3PzidslGXmPRsA@ds031691.mongolab.com:31691/heroku_6qs637vm'
-    'url' : 'mongodb://127.0.0.1/fleetclub'
-    //'url' : 'mongodb://heroku_fcr8cm1t:fQfbQr8JXcK1AME6jAID@ds043942.mongolab.com:43942/heroku_fcr8cm1t'
+    //'url' : 'mongodb://127.0.0.1/fleetclub'
+    'url' : 'mongodb://admin:ishaq123@ds159188.mlab.com:59188/heroku_xrqp5d7j'
 };
